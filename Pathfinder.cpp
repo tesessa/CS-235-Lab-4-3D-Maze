4f19bfdd0f9fb634d2809170dfe84454dcb0679e
Tessa Andersen <tessa@MacBook-Air-2.local>
2022-02-10 23:15:33 -0700

    Pathfinder.cpp

--- Pathfinder.cpp
+++ Pathfinder.cpp
@@ -15,7 +15,8 @@ Pathfinder::Pathfinder() {
 	}
 
 bool Pathfinder::importMaze(string file_name)
-	{	
+	{
+		int failure = 0;
 		int countOnes = 0;
 		int counter = 0;
 		cout << "importMaze from "<<file_name<<endl;
@@ -24,15 +25,16 @@ bool Pathfinder::importMaze(string file_name)
 			string line;
 			int row_count = 0;
 			for(int row = 0; row < ROW_SIZE; row++) {
-				for(int col = 0; col < COL_SIZE; col++) {
-					getline(file, line);
+			  for(int col = 0; col < COL_SIZE; col++) {
+					if(!getline(file, line)) failure=1;
 					stringstream ss(line);
 					for(int height = 0; height < HEIGHT_SIZE; height++) {
 					
 					int value;
+					if(ss.fail()) failure=1;
 					ss >> value;
 					if(value != 0 && value != 1) {
-						return false;
+						failure=1;
 					}
 					if (value == 1) {
 						countOnes++;
@@ -43,17 +45,29 @@ bool Pathfinder::importMaze(string file_name)
 					}
 				}
 				getline(file, line);
-			}
 		}
+
+		if(getline(file, line)) failure=1;
+		}
+
+
 		if(counter != 125) {
-			return false;
+			failure=1;
 		}
-		if(countOnes == 125) {
-			return false;
+		if (maze_grid[0][0][0] != 1 || temp[4][4][4] != 1) {
+			failure=1;
 		}
-		if (maze_grid[0][0][0] != 1 || maze_grid[4][4][4] != 1) {
-			return false;
+		if(failure==1) {
+		 for(int row = 0; row < ROW_SIZE; row++) {
+		  for(int col=0; col<COL_SIZE;col++) {
+		   for(int z=0; z<HEIGHT_SIZE; z++) {
+	//	   maze_grid[row][col][z] = 1;
+				return false;
 		}
+	}
+}
+}
+	    memcpy(maze_grid, temp, sizeof(maze_grid));
 	    return(true);
 	}
 
@@ -68,11 +82,19 @@ string Pathfinder::toString() const
 	    for(int row = 0; row < ROW_SIZE; row++) {
 	        for(int col = 0; col < COL_SIZE; col++) {
 		 for(int height = 0; height < HEIGHT_SIZE; height++) {
-	            ss << maze_grid[row][col][height];
+		 if(height != HEIGHT_SIZE -1) {
+	            ss << maze_grid[row][col][height] << " ";
+		    } else {
+		    ss << maze_grid[row][col][height];
 		    }
+		    }
+		  //  if(col < 4) {
 	        ss << endl;
-	    }
-	    ss << endl;
+	//	}
+	    	}
+	    		if(row < 4) {
+	    		ss << endl;
+	    	}
 	    }
 	    return ss.str();
 	}
@@ -80,7 +102,9 @@ string Pathfinder::toString() const
 
 
 
-	bool Pathfinder::find_maze_path(int grid[ROW_SIZE][COL_SIZE][HEIGHT_SIZE], int r, int c, int h) {
+bool Pathfinder::find_maze_path(int grid[ROW_SIZE][COL_SIZE][HEIGHT_SIZE], int r, int c, int h) {
+
+	
 	  cout << "find_maze_path ["<<r<<"]["<<c<<"]["<<h<<"]"<<endl;
 	  cout << this->toString();
 	  if (r < 0 || c < 0 || h < 0 || r >= ROW_SIZE || c >= COL_SIZE || h>=HEIGHT_SIZE)
@@ -89,7 +113,7 @@ string Pathfinder::toString() const
 	    return false;      // Cell is on barrier or dead end.
 	  else if (r == ROW_SIZE - 1 && c == COL_SIZE - 1 && h == HEIGHT_SIZE-1) {
 	    grid[r][c][h] = PATH;         // Cell is on path
-	    solution.push_back("("+to_string(r)+","+to_string(c)+","+to_string(h)+")");
+	    solution.push_back("("+to_string(r)+", "+to_string(c)+", "+to_string(h)+")");
 	    return true;               // and is maze exit.
 	  }
 	  else { 
@@ -97,6 +121,7 @@ string Pathfinder::toString() const
 	    // Attempt to find a path from each neighbor.
 	    // Tentatively mark cell as on path.
 	    grid[r][c][h] = PATH;
+	    solution.push_back("("+to_string(r)+", "+to_string(c)+", "+to_string(h)+")");
 	    if (find_maze_path(grid, r-1, c, h) // Up
 	        || find_maze_path(grid, r+1, c, h) // Down
 	        || find_maze_path(grid, r, c-1, h) // Left
@@ -104,11 +129,12 @@ string Pathfinder::toString() const
 		|| find_maze_path(grid, r, c, h+1) //forward
 		|| find_maze_path(grid, r, c, h-1) //Back
 		) {
-	      solution.push_back("("+to_string(r)+","+to_string(c)+","+to_string(h)+")");
 	      return true;
 	    }
 	    else {
+	    	
 	      grid[r][c][h] = TEMPORARY;  // Dead end.
+	      solution.pop_back();
 	      return false;
 	    }
 	  }
@@ -121,7 +147,8 @@ string Pathfinder::toString() const
 
 vector<string> Pathfinder::solveMaze()
 {
-	find_maze_path(maze_grid, 0,0,0);
+	find_maze_path(maze_grid, 0, 0, 0);
+	memcpy(temp, maze_grid, sizeof(maze_grid));
 	for(auto s:solution) {
 	cout <<s<<endl;
 	}
